05b80c4a38af67a68191c476ccc62fe9fc3d8f50
FEIZZER <2208146837@qq.com>
2021-08-29 15:07:27 +0800

    init: 初始化商品微服务模块

---
+++ mfmall-product/src/main/java/com/feizzer/mfmall/mfmallproduct/MfmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.feizzer.mfmall.mfmallproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MfmallProductApplication.class, args);
+    }
+
+}
---
+++ mfmall-product/src/test/java/com/feizzer/mfmall/mfmallproduct/MfmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feizzer.mfmall.mfmallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
